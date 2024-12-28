6abaab22908e0bdab695fbcbf41bcb61252e4629
Jeck0v <arnaud.fischer75@gmail.com>
2024-12-29 00:18:01 +0100

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     app.config['SECRET_KEY'] = 'dev'
+
+    # Configuration JWT
+    app.config['JWT_SECRET_KEY'] = 'your-jwt-secret-key'
+    jwt = JWTManager(app)
 
     from .routes import main_bp
     from .api import api
