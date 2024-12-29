bac3f1f2c41fd2167539e4e570eae918af272d57
Jeck0v <arnaud.fischer75@gmail.com>
2024-12-29 13:39:11 +0100

    merge Ãà 2 mainsÃ

--- config.py
+++ config.py
@@ -2,7 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = 'your-secret-key'
+<<<<<<< HEAD
     MONGO_URI = os.getenv('MONGO_URI', 'mongodb://root:pass@mongodb:27017/condorcy?authSource=admin')
+=======
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://root:pass@mongodb:27017/condorsy?authSource=admin')
+>>>>>>> b377c54 (merge Ãà 2 mainsÃ)
 
 class DevelopmentConfig(Config):
     DEBUG = True
