cbda55b4f50de90c13345696c26f6d521557fba4
Jeck0v <arnaud.fischer75@gmail.com>
2024-12-29 18:00:04 +0100

    ça marche pas mais si je le perd je vais devenir fou

---
+++ app/queries.py
@@ -0,0 +1,47 @@
+# requèete MongoDB pour UC10/11
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from datetime import datetime
+
+client = MongoClient("mongodb://mongodb:27017/")
+db = client["condorsy"]
+surveys_collection = db["surveys"]
+users_collection = db["users"]
+
+def get_top_surveys_by_participants():
+    pipeline = [
+        {"$unwind": "$reponses"},
+        {"$group": {
+            "_id": "$_id",
+            "question": {"$first": "$question"},
+            "participant_count": {"$sum": 1}
+        }},
+        {"$sort": {"participant_count": -1}}
+    ]
+    return list(surveys_collection.aggregate(pipeline))
+
+def get_votes_by_birth_year(survey_id):
+    survey = surveys_collection.find_one({"_id": ObjectId(survey_id)})
+    user_ids = [response["user_id"] for response in survey["reponses"]]
+    users = users_collection.find({"_id": {"$in": user_ids}})
+    current_year = datetime.now().year
+    age_to_birth_year = {user["_id"]: current_year - user["age"] for user in users}
+
+    birth_year_votes = {}
+    for response in survey["reponses"]:
+        birth_year = age_to_birth_year[response["user_id"]]
+        birth_year_votes.setdefault(birth_year, [])
+        birth_year_votes[birth_year].extend(response["reponse"])
+
+    return birth_year_votes
+
+def get_average_choices():
+    pipeline = [
+        {"$project": {"choice_count": {"$size": "$choix"}}},
+        {"$group": {
+            "_id": None,
+            "average_choices": {"$avg": "$choice_count"}
+        }}
+    ]
+    result = surveys_collection.aggregate(pipeline)
+    return result.next()["average_choices"]
--- config.py
+++ config.py
@@ -2,11 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = 'your-secret-key'
-<<<<<<< HEAD
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://root:pass@mongodb:27017/condorcy?authSource=admin')
-=======
     MONGO_URI = os.getenv('MONGO_URI', 'mongodb://root:pass@mongodb:27017/condorsy?authSource=admin')
->>>>>>> b377c54 (merge Ãà 2 mainsÃ)
 
 class DevelopmentConfig(Config):
     DEBUG = True
